f234895cfcca32f5e0e464217e8242cdc2671357
rgerraty <rgerraty@gmail.com>
2016-05-03 14:23:59 -0400

    prepare data for stan

--- hybrid_data_stan.R
+++ hybrid_data_stan.R
@@ -1,33 +1,30 @@
-hybrid_data<-read.csv2('~/Downloads/CardGame.txt')
+hybrid_data<-read.delim('~/Downloads/CardGame.txt')
+
+
 library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
 
 subs = unique(hybrid_data$Sub)
-
 NS = length(subs)
-
 MT=max(hybrid_data$Trial) 
-
-NT = matrix(0,NS)
-
+NT = array(0,NS)
 choice = array(0,c(NS,MT))
-
 rew = array(0,c(NS,MT))
 
 
 
 for (i in 1:NS) {
   NT[i] = nrow(subset(hybrid_data,Sub==subs[i]));
-  choice[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$DeckC - 1;
-
+  choice[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$DeckC;
   rew[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$Outcome;
-
 }
 
-standata = list(NS= NS, NC=2, MT=MT, NT= NT, choice=choice, rew=rew )
+#for skipping missed trials
+choice[is.na(choice)]<--1
+rew[is.na(rew)]<--1
 
-# run four chains in parallel
+standata = list(NS=NS, NC=2, MT=MT, NT= NT, choice=choice, rew=rew )
 
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-fit <- stan(file = 'hybrid_rl.stan', data = standata, iter = 1250, warmup = 250, chains = 4)
+fit <- stan(file = '~/GitHub/hybrid_reinforcement_learning/hybrid_rl.stan', data = standata, iter = 1250, warmup = 250, chains = 4)
 
