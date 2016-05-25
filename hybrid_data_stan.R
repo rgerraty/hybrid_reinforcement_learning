ae90f1f0db919841a932b41a387dc9c172fecc9b
rgerraty <rgerraty@gmail.com>
2016-05-25 18:49:17 -0400

    prepare data for stan

--- hybrid_data_stan.R
+++ hybrid_data_stan.R
@@ -5,6 +5,21 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
+#abs(choice-3) is a hack to get unchosen from chosen [1,2];
+hybrid_data$DeckUnC<-abs(hybrid_data$DeckC-3)
+
+hybrid_data$ObjPP<-hybrid_data$ObjPP-.5
+
+hybrid_data$OldObjC[hybrid_data$OldObjC==0]<--1
+
+hybrid_data$OldObjC[is.na(hybrid_data$OldObjC)]<-0
+
+hybrid_data$ObjPP=hybrid_data$ObjPP*hybrid_data$OldObjC
+
+hybrid_data$ObjPP[is.na(hybrid_data$ObjPP)]<-0
+
+
+#set up variables in subjects by trials format for Stan
 subs = unique(hybrid_data$Sub);
 NS = length(subs);
 MT=max(hybrid_data$Trial);
@@ -15,15 +30,15 @@ rew = array(0.0,c(NS,MT));
 old_choice = array(0.0,c(NS,MT));
 old_val = array(0.0,c(NS,MT));
 
-
+#convert data to subjects by trials format for Stan
 for (i in 1:NS) {
   NT[i] = nrow(subset(hybrid_data,Sub==subs[i]));
   choice[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$DeckC;
   #abs(choice-3) is a hack to get unchosen from chosen;
-  unchoice[i,1:NT[i]] = abs(choice[i,1:NT[i]]-3);
+  unchoice[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$DeckUnC;
   rew[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$Outcome;
   old_choice[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$OldObjC;
-  old_val[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$ObjPP-.5;
+  old_val[i,1:NT[i]] = subset(hybrid_data,Sub==subs[i])$ObjPP;
 }
 
 #for skipping missed trials
@@ -32,10 +47,10 @@ choice[choice==0]<--1
 unchoice[is.na(unchoice)]<--1
 unchoice[choice==0]<--1
 rew[is.na(rew)]<--1
-old_choice[old_choice==0]<--1
-old_choice[is.na(old_choice)]<-0
-old_val=old_val*old_choice;
-old_val[is.na(old_val)]<-0
+
+
+
+
 
 
 standard_standata = list(NS=NS, NC=2, MT=MT, NT= NT, choice=choice, unchoice=unchoice, rew=rew )
