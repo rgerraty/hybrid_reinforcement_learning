7941def106854336cc5322faed736c03963eb787
rgerraty <rgerraty@gmail.com>
2016-06-05 12:38:10 -0400

    prepare data for stan

--- extract_RL_pars.R
+++ extract_RL_pars.R
@@ -56,9 +56,9 @@ summary(fit_extract$b_mean)
 
 
 
-hist(beta_hyb[,3],xlab="Episodic Effect",main=NULL)
-hist(beta_hyb[,1],xlab="Incremental Effect",main=NULL)
-hist(fit_extract$Omega[,3,1],xlab="Episodic-Incremental Correlation",main=NULL)
+hist(beta_hyb[,4],xlab="Episodic Effect",main=NULL)
+hist(beta_hyb[,2],xlab="Incremental Effect",main=NULL)
+hist(fit_extract$Omega[,4,2],xlab="Episodic-Incremental Correlation",main=NULL)
 hist(alpha_hyb,xlab="Learning Rate",main=NULL)
 
 plot(hybrid1_fit,pars=c("beta[1,1]","beta[2,1]",
@@ -77,21 +77,21 @@ plot(hybrid1_fit,pars=c("beta[1,1]","beta[2,1]",
                         "beta[27,1]","beta[28,1]",
                         "beta[29,1]","beta[30,1]","beta[31,1]"))
 
-plot(hybrid1_fit,pars=c("beta[1,3]","beta[2,3]",
-                        "beta[3,3]","beta[4,3]",
-                        "beta[5,3]","beta[6,3]",
-                        "beta[7,3]","beta[8,3]",
-                        "beta[9,3]","beta[10,3]",
-                        "beta[11,3]","beta[12,3]",
-                        "beta[13,3]","beta[14,3]",
-                        "beta[15,3]","beta[16,3]",
-                        "beta[17,3]","beta[18,3]",
-                        "beta[19,3]","beta[20,3]",
-                        "beta[21,3]","beta[22,3]",
-                        "beta[23,3]","beta[24,3]",
-                        "beta[25,3]","beta[26,3]",
-                        "beta[27,3]","beta[28,3]",
-                        "beta[29,3]","beta[30,3]","beta[31,3]"))
+plot(hybrid1_fit,pars=c("beta[1,4]","beta[2,4]",
+                        "beta[3,4]","beta[4,4]",
+                        "beta[5,4]","beta[6,4]",
+                        "beta[7,4]","beta[8,4]",
+                        "beta[9,4]","beta[10,4]",
+                        "beta[11,4]","beta[12,4]",
+                        "beta[13,4]","beta[14,4]",
+                        "beta[15,4]","beta[16,4]",
+                        "beta[17,4]","beta[18,4]",
+                        "beta[19,4]","beta[20,4]",
+                        "beta[21,4]","beta[22,4]",
+                        "beta[23,4]","beta[24,4]",
+                        "beta[25,4]","beta[26,4]",
+                        "beta[27,4]","beta[28,4]",
+                        "beta[29,4]","beta[30,4]","beta[31,4]"))
 
 
 
