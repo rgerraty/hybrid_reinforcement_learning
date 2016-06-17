8ee3ba2d150ba3ab7e412e8c5357c5883891d5cd
rgerraty <rgerraty@gmail.com>
2016-06-17 17:37:07 -0400

    modified scripts for running hybrid analyses on yeti cluster with qsub

--- yeti/run_1st_level_sub.sh
+++ yeti/run_1st_level_sub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Directives
-#PBS -N bedpost
+#PBS -N 1st_level_GLM
 #PBS -W group_list=yetipsych 
 #PBS -l nodes=2,walltime=40:00:00,mem=1024mb
 #PBS -M rtg2116@columbia.edu
@@ -12,14 +12,7 @@
 #PBS -o localhost:/vega/psych/users/rtg2116/outputs
 #PBS -e localhost:/vega/psych/users/rtg2116/errors
 
-# Print "Hello World"
 export FSLPARALLEL=1
 
 /u/6/r/rtg2116/GitHub/hybrid_reinforcement_learning/yeti/run_1st_level.sh $arg1 $arg2
 
-# Sleep for 10 seconds
-
-
-# Print date and time
-
-# End of script
--- yeti/run_preproc_sub.sh
+++ yeti/run_preproc_sub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Directives
-#PBS -N bedpost
+#PBS -N image preprocessing
 #PBS -W group_list=yetipsych 
 #PBS -l nodes=2,walltime=40:00:00,mem=1024mb
 #PBS -M rtg2116@columbia.edu
@@ -12,14 +12,8 @@
 #PBS -o localhost:/vega/psych/users/rtg2116/
 #PBS -e localhost:/vega/psych/users/rtg2116/
 
-# Print "Hello World"
+
 export FSLPARALLEL=1
 
 /u/6/r/rtg2116/GitHub/hybrid_reinforcement_learning/yeti/run_preproc.sh $arg1 $arg2 $arg3
 
-# Sleep for 10 seconds
-
-
-# Print date and time
-
-# End of script
