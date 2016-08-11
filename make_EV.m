04ced07b85938a96f7cf51412dabe879c636a662
rgerraty <rgerraty@gmail.com>
2016-08-11 19:50:25 -0400

    make 3 column EV

--- make_EV.m
+++ make_EV.m
@@ -67,9 +67,14 @@ for r = runs
 	FB_weight = Performance.pay.outcome(valid_trials & Performance.cond.Run==r)';
 
 	%RL model regressors
-	FB_pe_weight=N(N(:,1)==sub & N(:,2)==r,32);
-	choice_Qdiff_weight=N(N(:,1)==sub & N(:,2)==r,31);
-	choice_Qchose_weight=N(N(:,1)==sub & N(:,2)==r,30);
+	FB_pe_weight=N(N(:,1)==sub & N(:,2)==r,35);
+	choice_Qdiff_weight=N(N(:,1)==sub & N(:,2)==r,34);
+	choice_Qchose_weight=N(N(:,1)==sub & N(:,2)==r,33);
+
+	%index old vs new choice
+	old_choice=(N(N(:,1)==sub & N(:,2)==r,15))==.5;
+	new_choice=(N(N(:,1)==sub & N(:,2)==r,15))==-.5;
+	no_choice=(N(N(:,1)==sub & N(:,2)==r,15))==0;
 
     %combine into FSL-style 3col regs
     choice_run = [choice_time, choice_duration, ones(size(choice_time,1),1)];
@@ -77,6 +82,18 @@ for r = runs
     FBpay_run = [FB_time, FB_duration, FB_weight];
     FB_run = [FB_time, FB_duration, ones(size(FB_time,1),1)];
     inval_run = [inval_time, inval_duration, ones(size(inval_time,1),1)];
+
+    choice_run_oldc=choice_run(old_choice,:);
+    choice_run_newc=choice_run(new_choice,:);
+    choice_run_noc=choice_run(no_choice,:);
+
+    FB_oldc=FB_run(old_choice,:);
+    FB_newc=FB_run(new_choice,:);
+    FB_noc=FB_run(no_choice,:);
+
+    FBpay_oldc=FBpay_run(old_choice,:);
+    FBpay_newc=FBpay_run(new_choice,:);
+    FBPay_noc=FBpay_run(no_choice,:);
 	
 
 	%episodic EVs
@@ -91,13 +108,20 @@ for r = runs
 		oldnew_run=[choice_run(:,1:2) oldnew_weight'];	
 		oldval_run=[choice_run(:,1:2) oldval_weight'];
 		submem_run=[choice_run(:,1:2) submem_weight'];
+		oldval_oldc=oldval_run(old_choice,:);
+		oldval_newc=oldval_run(new_choice,:);
+
 		oldnew_run(choice_time<0,:)=[];
 		oldval_run(choice_time<0 | isnan(oldval_run(:,3)),:)=[];
+		oldval_oldc(choice_time<0 | isnan(oldval_oldc(:,3)),:)=[];
+		oldval_newc(choice_time<0 | isnan(oldval_newc(:,3)),:)=[];
 		submem_run(choice_time<0 | isnan(submem_run(:,3)),:)=[];
 
 		%write out EV txt files for episodic predictors
 		dlmwrite(strcat('EV_files/oldnew_run',num2str(r),'.txt'),oldnew_run, 'delimiter',' ');
 		dlmwrite(strcat('EV_files/oldval_run',num2str(r),'.txt'),oldval_run, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/oldval_oldc_run',num2str(r),'.txt'),oldval_oldc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/oldval_newc_run',num2str(r),'.txt'),oldval_newc, 'delimiter',' ');
 		dlmwrite(strcat('EV_files/submem_run',num2str(r),'.txt'),submem_run, 'delimiter',' ');
 	end
 
@@ -106,22 +130,59 @@ for r = runs
 		FB_pe_run = [FB_time, FB_duration, FB_pe_weight(valid_trials(Performance.cond.Run==r))];
 		choice_Qdiff_run=[choice_time, choice_duration, choice_Qdiff_weight(valid_trials(Performance.cond.Run==r))];
 		choice_Qchose_run=[choice_time, choice_duration, choice_Qchose_weight(valid_trials(Performance.cond.Run==r))];
+
+		FB_pe_oldc=FB_pe_run(old_choice,:);
+		FB_pe_newc=FB_pe_run(new_choice,:);
+		FB_pe_noc=FB_pe_run(no_choice,:);
+		choice_Qdiff_oldc=choice_Qdiff_run(old_choice,:);
+		choice_Qdiff_newc=choice_Qdiff_run(new_choice,:);
+		choice_Qdiff_noc=choice_Qdiff_run(no_choice,:);
+		choice_Qchose_oldc=choice_Qchose_run(old_choice,:);
+		choice_Qchose_newc=choice_Qchose_run(new_choice,:);
+		choice_Qchose_noc=choice_Qchose_run(no_choice,:);
+
 		
 		FB_pe_run(FB_time<0 | isnan(FB_pe_run(:,3)),:)=[];
 		choice_Qdiff_run(choice_time<0 | isnan(choice_Qdiff_run(:,3)),:)=[];
 		choice_Qchose_run(choice_time<0 | isnan(choice_Qchose_run(:,3)),:)=[];
+		FB_pe_oldc(FB_time<0 | isnan(FB_pe_oldc(:,3)),:)=[];
+		FB_pe_newc(FB_time<0 | isnan(FB_pe_newc(:,3)),:)=[];
+		FB_pe_noc(FB_time<0 | isnan(FB_pe_noc(:,3)),:)=[];
+		choice_Qdiff_oldc(choice_time<0 | isnan(choice_Qdiff_oldc(:,3)),:)=[];
+		choice_Qdiff_newc(choice_time<0 | isnan(choice_Qdiff_newc(:,3)),:)=[];
+		choice_Qdiff_noc(choice_time<0 | isnan(choice_Qdiff_noc(:,3)),:)=[];
+		choice_Qchose_oldc(choice_time<0 | isnan(choice_Qchose_oldc(:,3)),:)=[];
+		choice_Qchose_newc(choice_time<0 | isnan(choice_Qchose_newc(:,3)),:)=[];
+		choice_Qchose_noc(choice_time<0 | isnan(choice_Qchose_noc(:,3)),:)=[];;
+
 
 		dlmwrite(strcat('EV_files/FB_pe_run',num2str(r),'.txt'),FB_pe_run, 'delimiter',' ');
 		dlmwrite(strcat('EV_files/choice_Qdiff_run',num2str(r),'.txt'),choice_Qdiff_run, 'delimiter',' ');
 		dlmwrite(strcat('EV_files/choice_Qchose_run',num2str(r),'.txt'),choice_Qchose_run, 'delimiter',' ');
+		
+		dlmwrite(strcat('EV_files/FB_pe_oldc_run',num2str(r),'.txt'),FB_pe_oldc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/FB_pe_newc_run',num2str(r),'.txt'),FB_pe_newc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/FB_pe_noc_run',num2str(r),'.txt'),FB_pe_noc, 'delimiter',' ');
+
+		dlmwrite(strcat('EV_files/choice_Qdiff_oldc_run',num2str(r),'.txt'),choice_Qdiff_oldc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/choice_Qdiff_newc_run',num2str(r),'.txt'),choice_Qdiff_newc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/choice_Qdiff_noc_run',num2str(r),'.txt'),choice_Qdiff_noc, 'delimiter',' ');
+
+		dlmwrite(strcat('EV_files/choice_Qchose_oldc_run',num2str(r),'.txt'),choice_Qchose_oldc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/choice_Qchose_newc_run',num2str(r),'.txt'),choice_Qchose_newc, 'delimiter',' ');
+		dlmwrite(strcat('EV_files/choice_Qchose_noc_run',num2str(r),'.txt'),choice_Qchose_noc, 'delimiter',' ');
+
 	end
 
+	%LEFT OFF HERE. ALSO NEED TO GO BACK AND FIX TIMINGS FOR old choice files
 	%remove trials from deleted volumes
 	choice_run(choice_time<0,:)=[];
 	response_run(response_time<0,:)=[];
 	FBpay_run(FB_time<0,:)=[];
 	FB_run(FB_time<0,:)=[];
 	inval_run(inval_time<0,:)=[];
+
+
 	
 	if size(inval_run,1)==0
 		inval_run=[0, 0, 0];
